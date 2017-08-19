6a5d050144cccaf4214264ab5eea3c0106374919
Souji Kenzaki <chenuki@yahoo.co.jp>
2017-08-19 19:59:24 +0900

    コメント修正

--- ChainSkill/WeaponMorphSkill.js
+++ ChainSkill/WeaponMorphSkill.js
@@ -11,6 +11,7 @@
  * After this skill activates, the equipped weapon will morph into weapon with [weaponID].
  * Please note that if the user cannot meet equipment requirement, that weapon will also be unequipped.
  * If you set [weaponID] as 9999, you will morph the weapon into unequipped state, thus destorying it.
+ * the weapon changed this way will be reverted at end of the battle.
  *
  * <unequip_w>
  * After this skill activates, the equipped weapon will be unequipped.
@@ -31,12 +32,14 @@
  * このスキルが発動した「後」、装備している武器が[武器ID]の物に変化します。
  * キャラクターが元々その武器を装備できない場合、その武器は解除されますのでご注意ください。 
  * 尚、[武器ID]を9999とする事で、未装備の状態に変化させる…つまり、装備している武器を完全に破壊／消滅させる事が出来ます。
+ * また、現状に於いて、この様なプロセスで変化された武器は、戦闘終了時に元に戻ります。
  *
  * <unequip_w>
  * このスキルが発動した「後」、装備している武器が解除され、アイテム欄に戻ります。
  * 
  * 尚、上記二つのタグを同時に使用した場合、<unequip_w>が先に発動します。
  * つまり、現在装備している武器を解除してアイテム欄に戻した上で、新たにアイテムを生成し、装備する…と言う形になります。
+ * 尚、プラグイン仕様上、戦闘中に装備を変更できるプラグインとは相性が悪いです。ご注意ください。
  */
 
 (function() {
@@ -89,7 +92,7 @@
         if (chara.origWeapon)
         {
            chara.forceChangeEquip(0, chara.origWeapon);
-     chara.origWeapon = "";
+           chara.origWeapon = "";
         }
       });
   };
