981d4bf27b530a28ce3181ac530f173fa8afe963
Souji Kenzaki <chenuki@yahoo.co.jp>
2017-09-04 19:46:02 +0900

    暫定公開

---
+++ ChainSkill/WeaponEvolve.js
@@ -0,0 +1,125 @@
+/*:ja
+ * @plugindesc 武器進化プラグイン
+ * @author 剣崎宗二
+ *
+ *
+ * @help このプラグインにはプラグインコマンドはありません。
+ *
+ * 武器メモ欄用
+ * <weapon_evolve:[進化先武器ID],[使用回数]>
+ * 使用回数回分行動した際、武器が進化先武器IDに変化する。
+ * 尚このカウントはキャラ別にカウントされ、同じキャラクターであれば武器をはずしてもう一度つけてもリセットされませんが、他のキャラクターにつけた場合
+ *　別カウントとなります。
+ *
+ * スキルメモ欄用
+ * <non_weapon_atk>
+ * このスキルは武器を使わないと言うことを示すタグ。このタグが付いたスキルを使用した際、武器進化カウントは増えません。
+ * 
+ */
+
+(function() {
+  var BattleManager_startAction_kzk_weapon = BattleManager.startAction;
+  BattleManager.startAction = function() {
+    var subject = this._subject;
+
+    if (subject.isActor())
+    {
+       if (!subject.wEvolveList)
+       {
+          subject.wEvolveList = [];
+       }
+
+       var local = this;
+       subject.weapons().forEach(function(item)
+       {
+         local.setUpWeaponEvolve(subject, item);
+       });
+    }
+    
+    subject.isWeaponAttack = !(subject.currentAction().item().meta.non_weapon_atk);
+    console.log(BattleManager_startAction_kzk_weapon);
+    BattleManager_startAction_kzk_weapon.call(this);
+    console.log("done");
+  };
+    
+    
+
+  BattleManager.setUpWeaponEvolve = function(subject, weapon){
+    var weaponEvolve = weapon.meta.weapon_evolve;
+    if (weaponEvolve) {
+      var evolveparams = weaponEvolve.split(',');
+      var alreadyExist = false;
+      for (var i = 0; i < subject.wEvolveList.length; i++)
+      {
+          if(subject.wEvolveList[i].id == weapon.id)
+          {
+              alreadyExist = true;
+              break;
+          }
+      }
+      if (!alreadyExist)
+      {
+          var newEvolveEntry = {}
+          newEvolveEntry.id = weapon.id;
+          newEvolveEntry.targetItemId = evolveparams[0];
+          newEvolveEntry.currentCount = 0;
+          newEvolveEntry.maxCount = evolveparams[1];
+          subject.wEvolveList.push(newEvolveEntry);
+      }
+    }
+  }
+
+  var BattleManager_endAction_kzk_weapon = BattleManager.endAction;
+  BattleManager.endAction = function() {
+    var subject = this._subject;
+    var tempWeaponItem = null;
+    if (subject && subject.wEvolveList && subject.isWeaponAttack)
+    {
+      this.processWeaponEvolve(subject)
+    }
+   BattleManager_endAction_kzk_weapon.call(this)
+  };
+
+  BattleManager.processWeaponEvolve = function(subject)
+  {
+    var changeList = [];
+    var equipList = subject.equips();
+    for (var j = 0; j < equipList.length; j++)
+    {
+      if (DataManager.isWeapon(equipList[j]))
+      {
+         for (var i = 0; i < subject.wEvolveList.length; i++)
+         {
+            var currentListItem = subject.wEvolveList[i];
+            if (currentListItem.id == equipList[j].id)
+            {
+               currentListItem.currentCount++; if
+			    (currentListItem.currentCount >= currentListItem.maxCount)
+               {
+                  var targetItem = currentListItem.targetItemId >= 0 ? $dataWeapons[currentListItem.targetItemId] : null;
+                  this._logWindow.weaponMorphMsg(equipList[j], targetItem)
+                  subject.forceChangeEquip(j, targetItem);
+               }
+            }
+         }
+       }
+    };
+  }
+  
+  Window_BattleLog.prototype.weaponMorphMsg = function(currentItem, newItem) {
+    var text = '';
+    if (!newItem)
+    {
+      text = "装備していた%1が消滅した！"
+      this.push('addText', text.format(currentItem.name));
+    }
+    else
+    { 
+      text = "装備していた%1が%2に変化した！"
+      this.push('addText', text.format(currentItem.name, newItem.name));
+    }
+  }
+
+
+  
+})();
---
+++ Pictures/AnimatePicture.js
@@ -0,0 +1,104 @@
+﻿(function() {
+
+ImageManager.loadAnimePicture = function(filename, hue) {
+    return this.loadBitmap('img/animepictures/', filename, hue, true);
+};
+
+
+    var _Game_Interpreter_pluginCommand =
+            Game_Interpreter.prototype.pluginCommand;
+    Game_Interpreter.prototype.pluginCommand = function(command, args) {
+        _Game_Interpreter_pluginCommand.call(this, command, args);        
+        if (command === 'showAnimatePicture') {
+            $gameScreen.showAnimatePicture(args[4], args[5], args[6], args[0], args[1], args[2], args[3]);
+        }
+    };
+
+var Game_Picture_prototype_initTarget_kz = Game_Picture.prototype.initTarget;
+Game_Picture.prototype.initTarget = function() {
+    Game_Picture_prototype_initTarget_kz.call(this);
+    this._animateFrame = 0;
+    this._frameSpacing = 0;
+    this._animateRepeat = false;
+    this._origName = this._name;
+    this._currentFrame = 0;
+    this._fSpacingCount = 0;
+};
+
+var Game_Picture_prototype_update_kz = Game_Picture.prototype.update;
+Game_Picture.prototype.update = function() {
+    if (this._animateFrame > 0)
+    {
+      this.updateAnimateFrames();
+    }
+    Game_Picture_prototype_update_kz.call(this);
+};
+
+Game_Picture.prototype.setUpAnimateFrame = function(totalFrame, repeat, frameSpace) {
+    this._animateFrame = Number(totalFrame);
+    console.log(this._animateFrame);
+    this._animateRepeat = (repeat == 'true');
+    console.log(this._animateRepeat);
+    this._frameSpacing = Number(frameSpace) ? Number(frameSpace) : 0;
+    console.log(this._frameSpacing);
+};
+
+function util_fill3_0(n, d) {
+    var m = n.toString(10);
+    while (m.length < d) {
+        m = "0" + m;
+    }
+    return m;
+}
+
+Game_Picture.prototype.updateAnimateFrames = function() {
+    this._fSpacingCount++;
+    if (this._fSpacingCount > this._frameSpacing)
+    {
+       this._fSpacingCount = 0;
+       if (this._currentFrame > this._animateFrame)
+       {
+          this._currentFrame = 0;
+          if (!this._animateRepeat)
+          {
+              this._animateFrame = 0;
+              this._animateRepeat = false;
+              this._frameSpacing = 0;
+          }
+       }
+       this._name = this._origName + '_' + util_fill3_0(this._currentFrame, 3);
+       this._currentFrame++;
+    }
+};
+
+Game_Screen.prototype.showAnimatePicture = function(totalFrame, repeat, frameSpace, pictureId, name, x, y) {
+    this.showPicture(pictureId, name, 0, x, y, 30, 30, 255, 0);
+    var realPictureId = this.realPictureId(pictureId);
+    for (var i = 0; i <= totalFrame; i++)
+    {
+       ImageManager.loadAnimePicture(name + '_' + util_fill3_0(i, 3))
+    }
+    var local = this;
+    var waiter = setInterval(function(){ //定期的に繰り返されるIntervalイベント登録
+    if (ImageManager.isReady())
+    {
+       local._pictures[realPictureId].setUpAnimateFrame(totalFrame, repeat, frameSpace);
+       clearInterval(waiter);  //executeDrawFaceがtrueを返した場合、Interval消去
+    }
+    }, 100);　//0.1秒間隔
+};
+
+var Sprite_Picture_prototype_loadBitmap = Sprite_Picture.prototype.loadBitmap;
+Sprite_Picture.prototype.loadBitmap = function() {
+    var animated = this.picture()._animateFrame > 0;
+    if (animated)
+    {
+      this.bitmap = ImageManager.loadAnimePicture(this._pictureName);
+    }
+    else
+    {
+      Sprite_Picture_prototype_loadBitmap.call(this);
+    }
+};
+
+})();
