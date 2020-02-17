25775bd7c0ae7445192a762af60fd0169bee6bbb
Souji Kenzaki <chenuki@yahoo.co.jp>
2020-02-18 00:53:06 +0900

    アニメーション機能

--- Pictures/kz_PictureChoices.js
+++ Pictures/kz_PictureChoices.js
@@ -18,7 +18,16 @@
  * @min -99999
  * @desc カーソル位置調整座標Y
  * @default 0
+ * 
+ * @param Cursor Max Frame
+ * @type number
+ * @desc カーソルアニメーションの最大フレーム数（0でアニメーションなし）
+ * @default 0
  *
+ * @param Cursor Frame Spacing
+ * @type number
+ * @desc カーソルアニメーションのフレーム間隔
+ * @default 0
  * 
  * @help
  * 有効化はプラグインコマンド GraphicalChoice on
@@ -37,6 +46,11 @@
  * 尚、キーボード操作はデフォルトに準じ、上下キーでのみカーソルが動かせる事にご注意ください。
  * タッチ／クリック操作はこの制約を受けません。
  * 
+ * Cursor Max Frameを1以上にする事で、カーソルをアニメーションさせる事が可能です。
+ * この場合、例えばCursor Bitmap Nameが cs
+ * Cursor Max Frameが3だった場合
+ * cs0.png~cs3.pngまでが順に再生されその後cs0.pngに巻き戻りループします。
+ * 
  * 
  * 
  * 開発協力Special Thanks：
@@ -52,6 +66,8 @@
     var _cursorBmp = String(parameters['Cursor Bitmap Name'] || "");
     var _cursorX = Number(parameters['Cursor Overhead X']) || 0;
     var _cursorY = Number(parameters['Cursor Overhead Y']) || 0;
+    var _c_maxFrame = Number(parameters['Cursor Max Frame']) || 0;
+    var _c_Interval = Number(parameters['Cursor Frame Spacing']) || 0;
 
     var _Game_Interpreter_pluginCommand =
         Game_Interpreter.prototype.pluginCommand;
@@ -87,8 +103,7 @@
         kz_Window_ChoiceList_prototype_initialize.call(this, messageWindow);
 
         this._choiceSprite = [];
-        this._cursorSprite = new Sprite_Base();
-        this._cursorSprite.bitmap = ImageManager.loadChoices(_cursorBmp);
+        this._cursorSprite = new Sprite_ChoiceCursor(_c_maxFrame, _c_Interval);
     };
 
     var kz_Window_ChoiceList_prototype_start = Window_ChoiceList.prototype.start;
@@ -185,5 +200,62 @@
         }
         return true;
     };
+
+    function Sprite_ChoiceCursor() {
+        this.initialize.apply(this, arguments);
+    }
+    
+    Sprite_ChoiceCursor.prototype = Object.create(Sprite_Base.prototype);
+    Sprite_ChoiceCursor.prototype.constructor = Sprite_Weapon;
+    
+    Sprite_ChoiceCursor.prototype.initialize = function(cursorMaxFrame, cursorInterval) {
+        Sprite_Base.prototype.initialize.call(this);
+
+        if (!cursorMaxFrame)
+        {
+            this.bitmap = ImageManager.loadChoices(_cursorBmp);
+        }
+
+        this._maxFrame = cursorMaxFrame;
+        this._interval = cursorInterval;
+        this._currentFrame = 0;
+        this._currentInterval = 0;
+        this._currentCursorPicName = "";
+    };
+
+    Sprite_ChoiceCursor.prototype.update = function() {
+        Sprite_Base.prototype.update.call(this);
+        this.updateCursorFrameAnimation();
+    };
+
+    Sprite_ChoiceCursor.prototype.updateCursorFrameAnimation = function() {
+        if (!this._maxFrame) return;
+        console.log(this._currentInterval + "/" + this._interval)
+
+        if (this._currentInterval > this._interval)
+        {
+            console.log("switch");
+            this._currentFrame++;
+            this._currentInterval = 0;
+        }
+
+        if (this._currentFrame > this._maxFrame)
+        {
+            this._currentFrame = 0;
+        }
+
+        if (this._currentCursorPicName != this.cursorPicName())
+        {
+            this.bitmap = ImageManager.loadChoices(this.cursorPicName());
+            this._currentCursorPicName = this.cursorPicName();
+        }
+
+        this._currentInterval++;
+    };
+
+    Sprite_ChoiceCursor.prototype.cursorPicName = function() {
+        return _cursorBmp + String(this._currentFrame);
+    }
+
 
 })();
